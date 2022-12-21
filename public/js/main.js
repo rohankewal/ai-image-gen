0c407f356edf5dda95629af158fafd86b818920f
Rohan Kewalramani <rkewalramani4@gmail.com>
2022-12-21 14:54:40 -0500

    added the ability to select art medium and subject

--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,65 @@
 function onSubmit(e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  document.querySelector('.msg').textContent = '';
-  document.querySelector('#image').src = '';
+	document.querySelector('.msg').textContent = '';
+	document.querySelector('#image').src = '';
 
-  const prompt = document.querySelector('#prompt').value;
-  const size = document.querySelector('#size').value;
+	const prompt = document.querySelector('#prompt').value;
+	const size = document.querySelector('#size').value;
 
-  if (prompt === '') {
-    alert('Please add some text');
-    return;
-  }
+	const subject = document.querySelector('#subject').value;
+	const medium = document.querySelector('#medium').value;
 
-  generateImageRequest(prompt, size);
+	// make a prompt text based on the user's input of the subject and medium(a painting of a waterfall in watercolor) and add it to the prompt field in the form
+	prompt += `A painting of a ${subject} in ${medium}`;
+
+	/* if (prompt === '') {
+		alert('Please add some text');
+		return;
+	} */
+
+	generateImageRequest(prompt, size);
 }
 
 async function generateImageRequest(prompt, size) {
-  try {
-    showSpinner();
+	try {
+		showSpinner();
 
-    const response = await fetch('/openai/generateimage', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        prompt,
-        size,
-      }),
-    });
+		const response = await fetch('/openai/generateimage', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				prompt,
+				size,
+			}),
+		});
 
-    if (!response.ok) {
-      removeSpinner();
-      throw new Error('That image could not be generated');
-    }
+		if (!response.ok) {
+			removeSpinner();
+			throw new Error('That image could not be generated');
+		}
 
-    const data = await response.json();
-    // console.log(data);
+		const data = await response.json();
+		// console.log(data);
 
-    const imageUrl = data.data;
+		const imageUrl = data.data;
 
-    document.querySelector('#image').src = imageUrl;
+		document.querySelector('#image').src = imageUrl;
 
-    removeSpinner();
-  } catch (error) {
-    document.querySelector('.msg').textContent = error;
-  }
+		removeSpinner();
+	} catch (error) {
+		document.querySelector('.msg').textContent = error;
+	}
 }
 
 function showSpinner() {
-  document.querySelector('.spinner').classList.add('show');
+	document.querySelector('.spinner').classList.add('show');
 }
 
 function removeSpinner() {
-  document.querySelector('.spinner').classList.remove('show');
+	document.querySelector('.spinner').classList.remove('show');
 }
 
 document.querySelector('#image-form').addEventListener('submit', onSubmit);

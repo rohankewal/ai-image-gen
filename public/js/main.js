342eab6d621b713342ed37c1e8c00be364014f6f
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-01-20 08:55:28 -0500

    made changes to wording and enabled editing

--- public/js/main.js
+++ public/js/main.js
@@ -1,89 +1,96 @@
 function onSubmit(e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	document.querySelector('.msg').textContent = '';
-	document.querySelector('#image').src = '';
+  let tokens = 20;
+  console.log("tokens:" + tokens);
 
-	const tokenDisplay = document.querySelector('#token-display');
-	const generateBtn = document.querySelector('#generate-btn');
+  let token_count = document.getElementById("tokens");
+  token_count.innerHTML = tokens;
 
-	let tokens = 20;
+  document.querySelector(".msg").textContent = "";
+  document.querySelector("#image").src = "";
 
-	let prompt = document.querySelector('#prompt').value;
-	let size = document.querySelector('#size').value;
+  let prompt = document.querySelector("#prompt").value;
+  let size = document.querySelector("#size").value;
 
-	let medium = document.querySelector('#medium').value;
-	let subject = document.querySelector('#subject').value;
-	let frame = document.querySelector('#frame').value;
+  let medium = document.querySelector("#medium").value;
+  let subject = document.querySelector("#subject").value;
+  let frame = document.querySelector("#frame").value;
 
-	prompt += `A ${subject} in ${medium} with a ${frame}`;
+  // TODO: Change prompt for more refined results
+  prompt += `${subject} in ${medium} with ${frame}`;
 
-	document.querySelector('#prompt').value = prompt;
+  if (frame == "no-frame") {
+    prompt += `${subject} in ${medium} without a frame`;
+  }
+  if (frame == "wood-frame") {
+    prompt += `${subject} in ${medium} with a wooden museum frame`;
+  }
+  if (frame == "metal-frame") {
+    prompt += `${subject} in ${medium} with a metal museum frame`;
+  }
+  if (frame == "modern-frame") {
+    prompt += `${subject} in ${medium} with a modern museum frame`;
+  }
 
-	/* if (prompt === '') {
+  tokens -= 1;
+  console.log("tokens:" + tokens);
+  token_count.innerHTML = tokens;
+
+  document.querySelector("#prompt").value = prompt;
+
+  /* if (prompt === '') {
 		alert('Please add some text');
 		return;
 	} */
 
-	if (subject === 'none' && medium === 'none') {
-		alert('Please select a subject and medium to generate an image');
-		return;
-	}
+  if (subject === "none" && medium === "none") {
+    alert("Please select a subject and medium to generate an image");
+    return;
+  }
 
-	generateBtn.addEventListener('click', () => {
-		tokens--;
-
-		tokenDisplay.innerHTML = `Tokens: ${tokens}`;
-
-		if (tokens > 0) {
-			generateImageRequest(prompt, size);
-		} else {
-			generateBtn.disabled = true;
-		}
-	});
-
-	// generateImageRequest(prompt, size);
+  generateImageRequest(prompt, size);
 }
 
 async function generateImageRequest(prompt, size) {
-	try {
-		showSpinner();
+  try {
+    showSpinner();
 
-		const response = await fetch('/openai/generateimage', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				prompt,
-				size,
-			}),
-		});
+    const response = await fetch("/openai/generateimage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+        size,
+      }),
+    });
 
-		if (!response.ok) {
-			removeSpinner();
-			throw new Error('That image could not be generated');
-		}
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error("That image could not be generated");
+    }
 
-		const data = await response.json();
-		// console.log(data);
+    const data = await response.json();
+    // console.log(data);
 
-		const imageUrl = data.data;
+    const imageUrl = data.data;
 
-		document.querySelector('#image').src = imageUrl;
+    document.querySelector("#image").src = imageUrl;
 
-		removeSpinner();
-	} catch (error) {
-		document.querySelector('.msg').textContent = error;
-	}
+    removeSpinner();
+  } catch (error) {
+    document.querySelector(".msg").textContent = error;
+  }
 }
 
 function showSpinner() {
-	document.querySelector('.spinner').classList.add('show');
+  document.querySelector(".spinner").classList.add("show");
 }
 
 function removeSpinner() {
-	document.querySelector('.spinner').classList.remove('show');
+  document.querySelector(".spinner").classList.remove("show");
 }
 
-document.querySelector('#image-form').addEventListener('submit', onSubmit);
+document.querySelector("#image-form").addEventListener("submit", onSubmit);
